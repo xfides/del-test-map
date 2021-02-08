389c8dd72956f87642d60a00c45aaf69d435041e
baradmitrij <baradmitrij@yandex.ru>
2021-02-08 20:20:09 +0200

    feat: start project. New folder\file structure

---
+++ src/module2.ts
@@ -0,0 +1,5 @@
+function sayHi(){
+  alert('sayHi');
+}
+
+export default sayHi;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'src')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        port: 9000, // разобраться почему не работает
+        open: true
+    }
+};
+
